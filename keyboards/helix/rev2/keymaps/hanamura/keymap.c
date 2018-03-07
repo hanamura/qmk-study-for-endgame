a7ab54651e8906588b480926721b43f5bbe21150
Taro Hanamura <m@hanamurataro.com>
2018-03-07 23:07:29 +0900

    Tidy

--- keyboards/helix/rev2/keymaps/hanamura/keymap.c
+++ keyboards/helix/rev2/keymaps/hanamura/keymap.c
@@ -17,16 +17,11 @@
 extern keymap_config_t keymap_config;
 
 #ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
 extern rgblight_config_t rgblight_config;
 #endif
 
 extern uint8_t is_master;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _QWERTY 0
 #define _COLEMAK 1
 #define _DVORAK 2
@@ -51,11 +46,9 @@ enum macro_keycodes {
   KC_SAMPLEMACRO,
 };
 
-
-// Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-//Macros
+
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -178,7 +171,6 @@ float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
 #endif
 
-// define variables for reactive RGB
 bool TOG_STATUS = false;
 int RGB_current_mode;
 
@@ -187,7 +179,6 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
-// Setting ADJUST layer RGB back to default
 void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
     #ifdef RGBLIGHT_ENABLE
