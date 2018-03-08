f2b2d2f1961d23862192a25c6f230e27d07a67b5
Taro Hanamura <m@hanamurataro.com>
2018-03-08 17:34:50 +0900

    Assign standalone Numpad key

--- keyboards/helix/rev2/keymaps/hanamura/keymap.c
+++ keyboards/helix/rev2/keymaps/hanamura/keymap.c
@@ -87,6 +87,7 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
+  NUMPAD,
   ADJUST,
   BACKLIT,
   RGBRST
@@ -132,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,                  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
       CTL_ESC, KC_A,     KC_S,    KC_D,    KC_F,   KC_G,                  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
       SFT_TAB, NUMPAD_Z, KC_X,    KC_C,    KC_V,   KC_B,                  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      M_CAG,   _______,  KC_LALT, KC_LALT, CMD_EN, LOWER, KC_SPC, KC_SPC, RAISE, CMD_JA, _______, _______, _______, _______  \
+      M_CAG,   _______,  KC_LALT, KC_LALT, CMD_EN, LOWER, NUMPAD, KC_SPC, RAISE, CMD_JA, _______, _______, _______, _______  \
       ),
 
   [_LOWER] = KEYMAP( \
@@ -234,6 +235,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_RAISE);
         TOG_STATUS = false;
         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case NUMPAD:
+      if (record->event.pressed) {
+        layer_on(_NUMPAD);
+      } else {
+        layer_off(_NUMPAD);
       }
       return false;
       break;
