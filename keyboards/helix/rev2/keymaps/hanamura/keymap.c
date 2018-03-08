752072c71a8d2687b254fb0c89566c8547a20211
Taro Hanamura <m@hanamurataro.com>
2018-03-08 12:28:51 +0900

    Add Numpad layer

--- keyboards/helix/rev2/keymaps/hanamura/keymap.c
+++ keyboards/helix/rev2/keymaps/hanamura/keymap.c
@@ -80,6 +80,7 @@ extern uint8_t is_master;
 #define _QWERTY 0
 #define _LOWER 3
 #define _RAISE 4
+#define _NUMPAD 5
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -119,16 +120,19 @@ enum custom_keycodes {
 #define TDL TD(TD_L)
 #define TDR TD(TD_R)
 
+// layers
+#define NUMPAD_Z LT(_NUMPAD,KC_Z)
+
 // utils
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP( \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
-      SFT_TAB, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      M_CAG,   _______, KC_LALT, KC_LALT, CMD_EN, LOWER, KC_SPC, KC_SPC, RAISE, CMD_JA, _______, _______, _______, _______  \
+      KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,                  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      CTL_ESC, KC_A,     KC_S,    KC_D,    KC_F,   KC_G,                  KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+      SFT_TAB, NUMPAD_Z, KC_X,    KC_C,    KC_V,   KC_B,                  KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      M_CAG,   _______,  KC_LALT, KC_LALT, CMD_EN, LOWER, KC_SPC, KC_SPC, RAISE, CMD_JA, _______, _______, _______, _______  \
       ),
 
   [_LOWER] = KEYMAP( \
@@ -143,6 +147,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, KC_TILD, KC_GRV,  KC_DQT,  KC_QUOT, KC_UNDS,                   KC_MINS, TDL,     TDR,     KC_LBRC, KC_RBRC, _______, \
       _______, _______, _______, C_TRUDQ, C_TRUSQ, KC_PLUS,                   KC_EQL,  C_ELPS,  KC_PIPE, KC_LCBR, KC_RCBR, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+      ),
+
+  [_NUMPAD] = KEYMAP( \
+      _______, _______, _______, KC_UP,   _______, _______,                   KC_PPLS, KC_P7,   KC_P8, KC_P9,   KC_P0,   _______, \
+      _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_PMNS, KC_P4,   KC_P5, KC_P6,   KC_PAST, _______, \
+      _______, _______, _______, _______, _______, _______,                   KC_PEQL, KC_P1,   KC_P2, KC_P3,   KC_PSLS, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, KC_PCMM, _______  \
       ),
 
   [_ADJUST] = KEYMAP( \
