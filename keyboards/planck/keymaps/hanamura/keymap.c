edf3b7233fa0e42aaf2f285f0359d385e71c7786
Taro Hanamura <m@hanamurataro.com>
2018-03-09 12:20:38 +0900

    Add Numpad key

--- keyboards/planck/keymaps/hanamura/keymap.c
+++ keyboards/planck/keymaps/hanamura/keymap.c
@@ -113,10 +113,10 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,     KC_W,    KC_E,     KC_R,  KC_T,    KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_BSPC},
-  {CTL_ESC, KC_A,     KC_S,    KC_D,     KC_F,  KC_G,    KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_ENT},
-  {SFT_TAB, NUMPAD_Z, KC_X,    KC_C,     KC_V,  KC_B,    KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT},
-  {M_CAG,   KC_LCTL,  KC_LALT, CMD_EISU, LOWER, KC_LSFT, KC_SPC, RAISE, CMD_KANA, KC_RALT, KC_RCTL, M_CAG}
+  {KC_TAB,  KC_Q,     KC_W,    KC_E,     KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_BSPC},
+  {CTL_ESC, KC_A,     KC_S,    KC_D,     KC_F,  KC_G,   KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_ENT},
+  {SFT_TAB, NUMPAD_Z, KC_X,    KC_C,     KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT},
+  {M_CAG,   KC_LCTL,  KC_LALT, CMD_EISU, LOWER, NUMPAD, KC_SPC, RAISE, CMD_KANA, KC_RALT, KC_RCTL, M_CAG}
 },
 
 [_LOWER] = {
@@ -180,6 +180,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case NUMPAD:
+      if (record->event.pressed) {
+        layer_on(_NUMPAD);
+      } else {
+        layer_off(_NUMPAD);
       }
       return false;
       break;
