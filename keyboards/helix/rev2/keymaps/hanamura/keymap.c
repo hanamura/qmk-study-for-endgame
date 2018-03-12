551197b68ed7d4fadb852e3b81eda6d152eccf23
Taro Hanamura <m@hanamurataro.com>
2018-03-12 19:29:02 +0900

    SandS

--- keyboards/helix/rev2/keymaps/hanamura/keymap.c
+++ keyboards/helix/rev2/keymaps/hanamura/keymap.c
@@ -133,10 +133,10 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP( \
-      KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,     KC_T,                   KC_Y,  KC_U,                            KC_I,          KC_O,    KC_P,    KC_BSPC,      \
-      CTL_ESC, KC_A,    TEXT_SELECT_S, TEXT_WALK_D, NUMPAD_F, KC_G,                   KC_H,  KC_J,                            KC_K,          KC_L,    KC_SCLN, KC_ENT,       \
-      SFT_TAB, KC_Z,    KC_X,          KC_C,        KC_V,     KC_B,                   KC_N,  KC_M,                            KC_COMM,       KC_DOT,  KC_SLSH, LSFT(KC_TAB), \
-      M_CAG,   _______, _______,       KC_LALT,     CMD_EN,   LOWER, KC_LSFT, KC_SPC, RAISE, MT(MOD_LGUI|MOD_LSFT, KC_LANG1), LGUI(KC_LALT), _______, _______, _______       \
+      KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,     KC_T,                                 KC_Y,  KC_U,                            KC_I,          KC_O,    KC_P,    KC_BSPC,      \
+      CTL_ESC, KC_A,    TEXT_SELECT_S, TEXT_WALK_D, NUMPAD_F, KC_G,                                 KC_H,  KC_J,                            KC_K,          KC_L,    KC_SCLN, KC_ENT,       \
+      SFT_TAB, KC_Z,    KC_X,          KC_C,        KC_V,     KC_B,                                 KC_N,  KC_M,                            KC_COMM,       KC_DOT,  KC_SLSH, LSFT(KC_TAB), \
+      M_CAG,   _______, _______,       KC_LALT,     CMD_EN,   LOWER, KC_LSFT, MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LGUI|MOD_LSFT, KC_LANG1), LGUI(KC_LALT), _______, _______, _______       \
       ),
 
   [_LOWER] = KEYMAP( \
