d6bbea9e68f469f8547e6139c221ee0ad43a4c09
Taro Hanamura <m@hanamurataro.com>
2018-05-21 15:36:33 +0900

    Reformat

--- keyboards/helix/rev2/keymaps/hanamura/keymap.c
+++ keyboards/helix/rev2/keymaps/hanamura/keymap.c
@@ -131,10 +131,10 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP( \
-      KC_TAB,  KC_Q,    KC_W,          KC_E,        KC_R,     KC_T,                   KC_Y,  KC_U,                  KC_I,          KC_O,    KC_P,    KC_BSPC,             \
-      MT(MOD_LCTL,KC_ESC), KC_A,    LT(_SELECT,KC_S), LT(_WALK,KC_D), LT(_NUM,KC_F), KC_G,                   KC_H,  KC_J,                  KC_K,          KC_L,    KC_SCLN, KC_ENT,              \
-      MT(MOD_LSFT,KC_TAB), KC_Z,    KC_X,          KC_C,        KC_V,     KC_B,                   KC_N,  KC_M,                  KC_COMM,       KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_TAB), \
-      LCTL(LALT(KC_LGUI)),   _______, _______,       KC_LALT,     MT(MOD_LGUI,KC_LANG2),   LOWER, KC_LSFT, KC_SPC, RAISE, MT(MOD_RGUI,KC_LANG1), LGUI(KC_LALT), _______, _______, _______              \
+      KC_TAB,              KC_Q,    KC_W,             KC_E,           KC_R,                  KC_T,                   KC_Y,  KC_U,                  KC_I,          KC_O,    KC_P,    KC_BSPC,             \
+      MT(MOD_LCTL,KC_ESC), KC_A,    LT(_SELECT,KC_S), LT(_WALK,KC_D), LT(_NUM,KC_F),         KC_G,                   KC_H,  KC_J,                  KC_K,          KC_L,    KC_SCLN, KC_ENT,              \
+      MT(MOD_LSFT,KC_TAB), KC_Z,    KC_X,             KC_C,           KC_V,                  KC_B,                   KC_N,  KC_M,                  KC_COMM,       KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_TAB), \
+      LCTL(LALT(KC_LGUI)), _______, _______,          KC_LALT,        MT(MOD_LGUI,KC_LANG2), LOWER, KC_LSFT, KC_SPC, RAISE, MT(MOD_RGUI,KC_LANG1), LGUI(KC_LALT), _______, _______, _______              \
       ),
 
   [_WALK] = KEYMAP( \
@@ -159,17 +159,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 
   [_LOWER] = KEYMAP( \
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR,  KC_ASTR,  _______,   KC_BSLS, _______, \
-      _______, KC_TILD, KC_GRV,  KC_DQT,  KC_QUOT, KC_UNDS,                   KC_MINS, TD(TD_LEFT), TD(TD_RIGHT), TD(TD_BOTH), _______, _______, \
-      _______, _______, _______, LALT(KC_LBRC), LALT(KC_RBRC), KC_PLUS,                   KC_EQL,  LALT(KC_SCLN),   KC_PIPE,  _______,   _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,   _______, _______  \
+      _______, KC_EXLM, KC_AT,   KC_HASH,       KC_DLR,        KC_PERC,                   KC_CIRC, KC_AMPR,       KC_ASTR,      _______,     KC_BSLS, _______, \
+      _______, KC_TILD, KC_GRV,  KC_DQT,        KC_QUOT,       KC_UNDS,                   KC_MINS, TD(TD_LEFT),   TD(TD_RIGHT), TD(TD_BOTH), _______, _______, \
+      _______, _______, _______, LALT(KC_LBRC), LALT(KC_RBRC), KC_PLUS,                   KC_EQL,  LALT(KC_SCLN), KC_PIPE,      _______,     _______, _______, \
+      _______, _______, _______, _______,       _______,       _______, _______, _______, _______, _______,       _______,      _______,     _______, _______  \
       ),
 
   [_RAISE] = KEYMAP( \
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR,  KC_ASTR,  _______,   KC_BSLS, _______, \
-      _______, KC_TILD, KC_GRV,  KC_DQT,  KC_QUOT, KC_UNDS,                   KC_MINS, TD(TD_LEFT), TD(TD_RIGHT), TD(TD_BOTH), _______, _______, \
-      _______, _______, _______, LALT(KC_LBRC), LALT(KC_RBRC), KC_PLUS,                   KC_EQL,  LALT(KC_SCLN),   KC_PIPE,  _______,   _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,   _______, _______  \
+      _______, KC_EXLM, KC_AT,   KC_HASH,       KC_DLR,        KC_PERC,                   KC_CIRC, KC_AMPR,       KC_ASTR,      _______,     KC_BSLS, _______, \
+      _______, KC_TILD, KC_GRV,  KC_DQT,        KC_QUOT,       KC_UNDS,                   KC_MINS, TD(TD_LEFT),   TD(TD_RIGHT), TD(TD_BOTH), _______, _______, \
+      _______, _______, _______, LALT(KC_LBRC), LALT(KC_RBRC), KC_PLUS,                   KC_EQL,  LALT(KC_SCLN), KC_PIPE,      _______,     _______, _______, \
+      _______, _______, _______, _______,       _______,       _______, _______, _______, _______, _______,       _______,      _______,     _______, _______  \
       ),
 
   [_BACKLIT] = KEYMAP( \
@@ -180,10 +180,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ),
 
   [_ADJUST] = KEYMAP( \
-      _______, _______, _______, KC_PGUP, KC_VOLU, LCTL(KC_F2),                     LCTL(KC_F5),   BACKLIT, LGUI(LSFT(LCTL(KC_4))), _______, _______, _______, \
-      _______, _______, _______, KC_PGDN, KC_VOLD, LGUI(LSFT(KC_LBRC)),                    LGUI(LSFT(KC_RBRC)),  QWERTY,  LGUI(LSFT(KC_4)),       _______, _______, _______, \
-      _______, RESET,   _______, _______, _______, LGUI(KC_LBRC),                   LGUI(KC_RBRC), _______, LGUI(LSFT(KC_3)),       _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______,   _______, _______, _______,   _______, _______,                _______, _______, _______  \
+      _______, _______, _______, KC_PGUP, KC_VOLU, LCTL(KC_F2),                           LCTL(KC_F5),         BACKLIT, LGUI(LSFT(LCTL(KC_4))), _______, _______, _______, \
+      _______, _______, _______, KC_PGDN, KC_VOLD, LGUI(LSFT(KC_LBRC)),                   LGUI(LSFT(KC_RBRC)), QWERTY,  LGUI(LSFT(KC_4)),       _______, _______, _______, \
+      _______, RESET,   _______, _______, _______, LGUI(KC_LBRC),                         LGUI(KC_RBRC),       _______, LGUI(LSFT(KC_3)),       _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______,             _______, _______, _______,             _______, _______,                _______, _______, _______  \
       )
 };
 
